e35ebd76cdcba7cf09ff48baf3ee744a9983e96a
xinrong789 <xinronglee@126.com>
2025-03-29 21:16:41 +1030

    delete incorrect code

--- src/components/navLeft/index.tsx
+++ src/components/navLeft/index.tsx
@@ -1,144 +1,76 @@
-// import { Menu } from "antd"
-// import { getMenu } from "../../api/users"
-// import { useState, useEffect } from "react"
-// import icons from "./iconList"
-// import logo from "../../assets/logo.png"
-// import "./index.scss"
-// import { setMenu } from "../../store/login/authSlice"
-// import { useDispatch } from "react-redux"
-// import { useNavigate, useLocation } from "react-router-dom"
-// import { useSelector } from "react-redux"
-
-// interface MenuItem {
-//   key: string
-//   label: string
-//   icon?: React.ReactNode
-//   children?: MenuItem[]
-// }
-// interface MenuItemFromData {
-//   key: string
-//   label: string
-//   icon: string
-//   children?: MenuItemFromData[]
-// }
-
-// function NavLeft() {
-//   const { menuList } = useSelector((state: any) => state.authSlice)
-//   // const dispatch = useDispatch()
-//   const navigate = useNavigate()
-//   const [menuData, setMenuData] = useState<MenuItem[]>([])
-//   const location = useLocation()
-//   // const selectedKey = location.pathname
-//   useEffect(() => {
-//     configMenu()
-//   }, [menuList])
-//   async function configMenu() {
-//     // const { data } = await getMenu()
-//     // dispatch(setMenu(data))
-//     const mappedMenuItems: MenuItem[] = mapMenuItems(menuList)
-//     setMenuData(mappedMenuItems)
-//   }
-
-//   function mapMenuItems(items: MenuItemFromData[]): any {
-//     return items.map((item: MenuItemFromData) => ({
-//       key: item.key,
-//       label: item.label,
-//       icon: icons[item.icon],
-//       children: item.children ? mapMenuItems(item.children) : null
-//     }))
-//   }
-
-//   function handleClick({ key }: { key: string }) {
-//     // console.log( key)
-//     navigate(key)
-//   }
-
-//   // const [selectedKeys, setSelectedKeys] = useState(["/dashboard"])
-//   return (
-//     <div className="navLeft">
-//       <div className="logo">
-//         <img src={logo} alt="" width={27} />
-//         <h1>New Era Smart Park</h1>
-//       </div>
-//       <Menu
-//         defaultSelectedKeys={["/dashboard"]}
-//         mode="inline"
-//         theme="dark"
-//         items={menuData}
-//         onClick={handleClick}
-//         // selectedKeys={selectedKeys}
-//         // selectedKeys={[location.pathname]}
-//       />
-//     </div>
-//   )
-// }
-
-// export default NavLeft
-
 import { Menu } from "antd";
+import { getMenu } from "../../api/users";
 import { useState, useEffect } from "react";
-import logo from "../../assets/logo.png";
 import icons from "./iconList";
+import logo from "../../assets/logo.png";
+import "./index.scss";
+import { setMenu } from "../../store/login/authSlice";
+import { useDispatch } from "react-redux";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-import "./index.scss";
 interface MenuItem {
   key: string;
   label: string;
   icon?: React.ReactNode;
   children?: MenuItem[];
 }
-
 interface MenuItemFromData {
   key: string;
   label: string;
   icon: string;
   children?: MenuItemFromData[];
 }
+
 function NavLeft() {
   const { menuList } = useSelector((state: any) => state.authSlice);
+  // const dispatch = useDispatch()
   const navigate = useNavigate();
   const [menuData, setMenuData] = useState<MenuItem[]>([]);
   const location = useLocation();
-  // const selectedKey=location.pathname
+  // const selectedKey = location.pathname
   useEffect(() => {
     configMenu();
   }, [menuList]);
   async function configMenu() {
+    // const { data } = await getMenu()
+    // dispatch(setMenu(data))
     const mappedMenuItems: MenuItem[] = mapMenuItems(menuList);
     setMenuData(mappedMenuItems);
   }
-  //将返回的菜单数据转换成我们需要的格式
+
   function mapMenuItems(items: MenuItemFromData[]): any {
     return items.map((item: MenuItemFromData) => ({
       key: item.key,
       label: item.label,
       icon: icons[item.icon],
-      children: item.children ? mapMenuItems(item.children) : null, //递归操作
+      children: item.children ? mapMenuItems(item.children) : null,
     }));
   }
 
   function handleClick({ key }: { key: string }) {
+    // console.log( key)
     navigate(key);
   }
 
+  // const [selectedKeys, setSelectedKeys] = useState(["/dashboard"])
   return (
-    <div className="navleft">
+    <div className="navLeft">
       <div className="logo">
-        <img src={logo} alt="" width={18} />
-        <h1>朋远智慧园区</h1>
+        <img src={logo} alt="" width={27} />
+        <h1>New Era Smart Park</h1>
       </div>
-
       <Menu
         defaultSelectedKeys={["/dashboard"]}
         mode="inline"
         theme="dark"
         items={menuData}
         onClick={handleClick}
-        selectedKeys={[location.pathname]}
+        // selectedKeys={selectedKeys}
+        // selectedKeys={[location.pathname]}
       />
     </div>
   );
 }
+
 export default NavLeft;
